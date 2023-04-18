f1119e32e8625ab08fae90f6fbeeae253e5f6427
Nathaniel Evan <nathanielevan@zohomail.com>
2023-04-19 01:37:54 +0700

    Handle invalid time and empty query result

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import psycopg2
+from datetime import datetime
 from flask import Flask
 from flask import request
 from flask import jsonify
@@ -21,6 +22,12 @@ def getstreamdata():
     social_media = request.args.get('social_media', default=None)
     if start_time is None or end_time is None:
         return "Invalid request", 500
+    else:
+        try:
+            datetime.strptime(start_time, '%Y-%m-%d %H:%M')
+            datetime.strptime(end_time, '%Y-%m-%d %H:%M')
+        except ValueError:
+            return "Invalid time parameters (must be in YYYY-MM-DD HH:MM format)", 500
     SQL_GET_STREAM_CMD = f"""SELECT timestamp, count, unique_count
                              FROM {dbtable}
                              WHERE timestamp BETWEEN \'{start_time}\' AND \'{end_time}\'"""
@@ -30,10 +37,13 @@ def getstreamdata():
     with dbconn.cursor() as cursor:
         cursor.execute(SQL_GET_STREAM_CMD)
         query_result = cursor.fetchone()
-    total_count = query_result[0]
-    total_unique_count = query_result[1]
+    if query_result is not None:
+        total_count = query_result[0]
+        total_unique_count = query_result[1]
+    else:
+        total_count = 0
+        total_unique_count = 0
     return jsonify(
             count=total_count,
             unique_count=total_unique_count
             )
-
