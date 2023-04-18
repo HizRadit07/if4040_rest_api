7d0c3274167f907419513306d866450d4173c7c6
Nathaniel Evan <nathanielevan@zohomail.com>
2023-04-18 19:30:41 +0700

    Initial commit - to be tested

---
+++ app.py
@@ -0,0 +1,38 @@
+import psycopg2
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+app = Flask(__name__)
+
+dbhost = "localhost"
+dbname = "stream"
+
+# connect to postgresql server
+dbconn = psycopg2.connect(
+        host=dbhost,
+        database=dbname)
+
+@app.get('/streamapi')
+def getstreamdata():
+    start_time = request.args.get('start')
+    end_time = request.args.get('end')
+    social_media = request.args.get('social_media', default=None)
+    if start_time is None or end_time is None:
+        return "Invalid request", 500
+    SQL_GET_STREAM_CMD = f"""SELECT SUM(count), SUM(unique_count)
+                             FROM {dbname}
+                             WHERE timestamp BETWEEN \'{start_time}\' AND \'{end_time}\'"""
+    if social_media is not None:
+        SQL_GET_STREAM_CMD += f""" AND social_media LIKE \'{social_media}\'"""
+    SQL_GET_STREAM_CMD += ";"
+    with dbconn.cursor() as cursor:
+        cursor.execute(SQL_GET_STREAM_CMD)
+        query_result = cursor.fetchone()
+    total_count = query_result[0]
+    total_unique_count = query_result[1]
+    return jsonify(
+            count=total_count,
+            unique_count=total_unique_count
+            )
+
